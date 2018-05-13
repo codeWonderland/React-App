708502e7ca938080d07620b98e8a108affb1bf3f
codeWonderland <alice.easter@mymail.champlain.edu>
2018-05-12 20:35:38 -0400

    Removed some unnecessary files, added some playground stuff

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class CompView extends React.Component {
@@ -12,19 +11,19 @@ class CompView extends React.Component {
     }
 }
 
+function NavView() {
+    return (
+      <div className={'nav-view'}/>
+    );
+}
+
 class App extends Component {
 
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
                 <CompView/>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+                <NavView/>
             </div>
         )
     }
