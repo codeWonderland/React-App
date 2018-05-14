9f23ae11b111c5e2d7af59fa76e382b29ef4c7f1
codeWonderland <alice.easter@mymail.champlain.edu>
2018-05-13 21:52:18 -0400

    SCSS Watchers and shit actually work now

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
 
+class Card extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { contents: this.props.contents }
+    }
+
+    render() { 
+        return (
+            <div className={'card'} >
+                { this.state.contents } 
+            </div>
+        )
+    }
+}
+
 class CompView extends React.Component {
     render() {
+
+        const cardContents = <p>I made a thing</p>
+
         return (
             <div className={'comp-view'} >
-                <p>I made a thing</p>
+                <Card contents={cardContents}/>
             </div>
         );
     }
